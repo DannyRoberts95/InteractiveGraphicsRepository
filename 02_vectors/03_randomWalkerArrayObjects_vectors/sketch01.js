442f3bae68edd34e3324dbe29afe4a3c61ccb5aa
Javawax <danielrdhr@gmail.com>
2018-01-28 18:07:18 +0000

    new object walkers

---
+++ 02_vectors/03_randomWalkerArrayObjects_vectors/sketch01.js
@@ -0,0 +1,58 @@
+var walkers =[];
+var population = 100;
+
+function setup(){
+	frameRate(100);
+	createCanvas(windowWidth,windowHeight);
+	background(255);
+	
+	for(i=0;i<population;i++){
+
+		walkers[i] = new Walker(random(0,width),random(0,height));
+	}
+}
+
+function draw(){
+
+	for(i=0;i<walkers.length;i++){
+
+		walkers[i].render();
+		walkers[i].move();
+	}
+
+}
+
+function Walker(x,y){
+
+	this.x=x;
+	this.y=y;
+
+	var position = createVector(this.x,this.y);
+
+	this.render = function(){
+		
+		
+		strokeWeight(5);
+		stroke(random(255),random(255),random(255));
+		point(position.x,position.y, 20, 20);
+	}
+
+	this.move = function(){
+		var choice = floor(random(4));
+
+		if(choice === 0){
+			position.x=position.x+5
+		}else if(choice === 1){
+			position.x=position.x-5
+		}else if(choice === 2){
+			position.y=position.y+5
+		}else{
+			position.y=position.y-5
+		}
+
+		constrain(position.x,0,width);
+		constrain(position.y,0,height);
+	}
+
+
+}
